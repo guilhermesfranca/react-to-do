40eced5dc8594e5e5ba42322b4743225c5c527cb
guilhermesfranca <guilhermesfranca@outlook.com>
2025-01-31 02:24:40 -0300

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/react-to-do',  // Nome do repositório
+  assetPrefix: '/react-to-do/',  // Nome do repositório
 };
 
 export default nextConfig;
